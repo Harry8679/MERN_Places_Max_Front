d1598e44199b3bfec2a8956dce7d24462df6d180
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-03 12:58:17 +0200

    New Place Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Users />} exact />
+        <Route path='/places/new' element={<NewPlace />} exact />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
