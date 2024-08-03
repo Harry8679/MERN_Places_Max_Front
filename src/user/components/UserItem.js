94f592a66bd00db7df1f61437cef992659bbb3c6
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-03 16:04:31 +0200

    Card Component and Link from react-router-dom library added

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './UserItem.css';
 import Avatar from '../../shared/components/UIElements/Avatar';
+import { Link } from 'react-router-dom';
+import Card from '../../shared/components/UIElements/Card';
 
 const UserItem = ({ id, image, name, placeCount }) => {
   return (
     <li className='user-item'>
       <div className="user-item__content">
-        <div className="user-item__image">
-            <Avatar image={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-            <h2>{name}</h2>
-            <h3>{placeCount} {placeCount === 1 ? 'Place': 'Places'}</h3>
-        </div>
+        <Card>
+            <Link to={`/${id}/places`}>
+                <div className="user-item__image">
+                    <Avatar image={image} alt={name} />
+                </div>
+                <div className="user-item__info">
+                    <h2>{name}</h2>
+                    <h3>{placeCount} {placeCount === 1 ? 'Place': 'Places'}</h3>
+                </div>
+            </Link>
+        </Card>
       </div>
     </li>
   )
